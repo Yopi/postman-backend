0fc3e40a7324f96bbfbfa27e6a901de2d16f6fa5
Jesper Bränn <brann.jesper@gmail.com>
2017-02-16 15:57:41 +0100

    As not keyword

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,4 @@
 class Request < ApplicationRecord
-  belongs_to :user, as: :owner, foreign_key: 'owner_id'
-  belongs_to :user, as: :courier, foreign_key: 'courier_id'
+  belongs_to :user, as :owner, foreign_key: 'owner_id'
+  belongs_to :user, as :courier, foreign_key: 'courier_id'
 end
