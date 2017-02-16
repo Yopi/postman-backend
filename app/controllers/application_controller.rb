16079495a1dbebd8af1466c98543de1a4f31ff85
Jesper Bränn <brann.jesper@gmail.com>
2017-02-16 15:47:13 +0100

    Add login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  include ActionController::Serialization
+
+  prepend_before_action :logged_in
+
+  def home
+    render json: {name: 'Postman API', api_version: '1.0'}.to_json
+  end
+
+  def current_user
+    @current_user
+  end
+
+  protected
+  def logged_in
+    user = nil
+    user, pass = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
+
+    if user.present? && pass.present?
+      is_authenticated = authenticate_or_request_with_http_basic do |email, password|
+        user = User.find_by_email(email)
+        user && user.authenticate(password)
+      end
+      @current_user = user if is_authenticated
+    end
+  end
+
+  def authenticate
+    return head 403 if current_user.blank?
+  end
 end
