1b29a65bccee4de82e3bd439063f3c31e1734884
Jesper Bränn <brann.jesper@gmail.com>
2017-02-17 09:02:03 +0100

    Make courier optional for request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
-  belongs_to :courier, class_name: 'User', foreign_key: 'courier_id'
+  belongs_to :courier, class_name: 'User', foreign_key: 'courier_id', optional: true
   belongs_to :parcel
 end
