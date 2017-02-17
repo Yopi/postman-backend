4715ca73f2700bb69a0c195823d5f2881a2c3da8
Jesper Bränn <brann.jesper@gmail.com>
2017-02-17 08:41:52 +0100

    Add telephones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216193240) do
+ActiveRecord::Schema.define(version: 20170217073201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,12 +50,13 @@ ActiveRecord::Schema.define(version: 20170216193240) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
+    t.string   "email",            null: false
     t.string   "password_digest"
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "first_name",       null: false
+    t.string   "last_name",        null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "telephone_number"
   end
 
 end
